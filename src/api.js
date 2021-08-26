88de5b987f3145f4e0bf3626d3df5f93b41f5bc3
Mihocsa Szilard <mihocsa48@gmail.com>
2021-08-26 10:53:34 +0200

    enable NProgress

--- src/api.js
+++ src/api.js
@@ -39,8 +39,9 @@ const extractLocations = (events) => {
 };
 
 const getEvents = async () => {
-  // NProgress.start();
+    NProgress.start();
 
+  // error: cannot read property 'start' of undefined - if isn't commented out 
   // // if (window.location.href.startsWith('http://localhost')) {
   // //   // NProgress.done();
   // //   return mockData;
@@ -57,7 +58,7 @@ const getEvents = async () => {
       localStorage.setItem("lastEvents", JSON.stringify(result.data));
       localStorage.setItem("locations", JSON.stringify(locations));
     }
-    // NProgress.done();
+      NProgress.done();
     return result.data.events;
   }
 };
