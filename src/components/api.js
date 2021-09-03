fa190ba20bc1dd0c8439c626d8fcaabed0dcc2aa
Mihocsa Szilard <mihocsa48@gmail.com>
2021-09-03 17:20:14 +0200

    code refactoring '->''

--- src/components/api.js
+++ src/components/api.js
@@ -47,9 +47,9 @@ const getEvents = async () => {
    }
 
   if (!navigator.onLine) {
-    const data = localStorage.getItem('lastEvents');
+    const data = localStorage.getItem("lastEvents");
     NProgress.done();
-    return data ? JSON.parse(data).events : [];;
+    return data?JSON.parse(data).events:[];;
    }
 
   const token = await getAccessToken();
